00aa2c7215afd36df00f5ccad0ceeb6ac551cb06
Jonathan Warner <jewarner57@gmail.com>
2021-06-14 12:36:44 -0400

    Added initial next block

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './style.css';
 
 import GridBoard from '../GridBoard/index'
+import NextBlock from '../NextBlock/index'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <NextBlock></NextBlock>
       <GridBoard></GridBoard>
     </div>
   );
