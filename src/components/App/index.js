9246b945f6e1a3145e4288ee5a4b75756421b626
Jonathan Warner <jewarner57@gmail.com>
2021-06-14 12:56:37 -0400

    feat: added store and provider

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 import './style.css';
 
 import GridBoard from '../GridBoard'
@@ -7,18 +11,22 @@ import ScoreBoard from '../ScoreBoard'
 import Controls from '../Controls'
 import MessagePopup from '../MessagePopup';
 
+const store = createStore(reducers)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris Redux</h1>
-      </header>
-      <NextBlock></NextBlock>
-      <GridBoard></GridBoard>
-      <ScoreBoard></ScoreBoard>
-      <Controls></Controls>
-      <MessagePopup></MessagePopup>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris Redux</h1>
+        </header>
+        <NextBlock></NextBlock>
+        <GridBoard></GridBoard>
+        <ScoreBoard></ScoreBoard>
+        <Controls></Controls>
+        <MessagePopup></MessagePopup>
+      </div>
+    </Provider>
   );
 }
 
